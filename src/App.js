36071a9e350b9fc3f0a1019dcd9cd480de99753d
Emanuel Arias <Emanuelarias9@gmail.com>
2025-03-23 22:59:05 -0500

    Proyecto React Peliculas - Maquetacion Inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="layout">
+      {/*Cabecera*/}
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <h1>React Peliculas</h1>
       </header>
+
+      {/*Barra de navegación*/}
+      <nav className="nav">
+        <ul>
+          <li>
+            <a href="/#">Inicio</a>
+          </li>
+          <li>
+            <a href="/#">Peliculas</a>
+          </li>
+          <li>
+            <a href="/#">Blog</a>
+          </li>
+          <li>
+            <a href="/#">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+
+      {/*Contenido principal*/}
+      <section id="content" className="content">
+        {/*aqui van las peliculas*/}
+        <article className="peli-item">
+          <h3 className="title">Desarrollo web</h3>
+          <p className="description"></p>
+
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+
+        <article className="peli-item">
+          <h3 className="title">Desarrollo web</h3>
+          <p className="description"></p>
+
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+
+        <article className="peli-item">
+          <h3 className="title">Desarrollo web</h3>
+          <p className="description"></p>
+
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+
+        <article className="peli-item">
+          <h3 className="title">Desarrollo web</h3>
+          <p className="description"></p>
+
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+      </section>
+
+      {/*Barra lateral*/}
+      <aside className="lateral">
+        <div className="search">
+          <h3 className="title">Buscador</h3>
+          <form>
+            <input type="text" id="search_field" />
+            <button id="search">Buscar</button>
+          </form>
+        </div>
+
+        <div className="add">
+          <h3 className="title">Añadir pelicula</h3>
+          <form>
+            <input type="text" id="title" placeholder="Titulo" />
+            <textarea id="description" placeholder="Descripción"></textarea>
+            <input type="submit" id="save" value="Guardar" />
+          </form>
+        </div>
+      </aside>
+
+      {/*Pie de página*/}
+      <footer className="footer"></footer>
     </div>
   );
 }
